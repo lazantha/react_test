b6fa37ec2b7d0b3b2535ca780d7939a12c6b03fb
lazantha <sglasanthapradeep@gmail.com>
2024-03-13 08:34:38 +0530

    route testing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
+import * as React from "react";
+import ReactDOM from "react-dom";
+import {
+  BrowserRouter as Router,
+  Routes, // Import Routes component
+  Route
+} from "react-router-dom";
+
 import logo from './logo.svg';
 import './App.css';
-import PartOne from './components/partOne.js'; // Capitalize component name
+import Header from './components/Header';
+import NodeList from './pages/PageList';
+import ContactPage from "./pages/ContactPage";
+import AboutUsPage from "./pages/AboutUsPage";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello from react js</h1>
-      <PartOne /> {/* Use capitalized component name */}
-      <PartOne />
-      <PartOne />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<NodeList />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/about" element={<AboutUsPage />} />
+          
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
